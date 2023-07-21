232cd614231b6cc61ec51ace3d08f4b0413eff05
Matt Perry <mpcomplete@gmail.com>
2023-07-21 15:12:26 -0400

    JumpPads fling player

--- Assets/Interactables/JumpPad.cs
+++ Assets/Interactables/JumpPad.cs
@@ -4,12 +4,16 @@ using UnityEngine;
 public class JumpPad : MonoBehaviour {
   public Hurtbox Hurtbox;
   public GameObject Model;
+  public Collider Collider;
+  public LayerMask LayerMask;
+  public Vector3 Extents = new Vector3(1, 1, 1);
   public Timeval SquashDuration = Timeval.FromSeconds(1.5f);
   public Vector3 SquashOffset = new Vector3(0, .15f, 0);
   int TicksRemaining = -1;
 
   private void Awake() {
     GetComponent<Combatant>().OnHurt += OnHurt;
+    Collider.enabled = true;
   }
 
   void OnHurt(HitEvent hit) {
@@ -21,10 +25,17 @@ public class JumpPad : MonoBehaviour {
   void Squash() {
     TicksRemaining = SquashDuration.Ticks;
     Model.transform.localPosition -= SquashOffset;
+    Collider.enabled = false;
   }
 
   void Popup() {
+    var hits = Physics.OverlapBox(transform.position, Extents, transform.rotation, LayerMask, QueryTriggerInteraction.Collide);
+    hits.ForEach(c => {
+      if (c.TryGetComponent(out Hurtbox hb))
+        hb.Owner.GetComponent<Rigidbody>().AddForce(7f * Vector3.up, ForceMode.VelocityChange);
+    });
     Model.transform.localPosition += SquashOffset;
+    Collider.enabled = true;
     // Fling Link
   }
 
