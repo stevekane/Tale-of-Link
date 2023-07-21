a6f9e681728a703be5d7c23c516524be1a189316
Matt Perry <mpcomplete@gmail.com>
2023-07-20 20:01:40 -0400

    Hammer

--- Assets/Core/Input/InputHandler.cs
+++ Assets/Core/Input/InputHandler.cs
@@ -7,6 +7,7 @@ public class InputHandler : MonoBehaviour {
 
   public Action<Vector3> OnMove;
   public Action OnSword;
+  public Action OnHammer;
 
   void Awake() {
     Inputs = new();
@@ -17,10 +18,13 @@ public class InputHandler : MonoBehaviour {
     Inputs.Dispose();
   }
 
+  // TODO: Task.Start runs async, so you could start 2 abilities on the same frame.
   void FixedUpdate() {
     var move = Inputs.Player.Move.ReadValue<Vector2>();
     OnMove?.Invoke(move.XZ());
     if (Inputs.Player.Sword.WasPerformedThisFrame())  // TODO: charging
       OnSword?.Invoke();
+    if (Inputs.Player.Hammer.WasPerformedThisFrame())
+      OnHammer?.Invoke();
   }
 }
--- Assets/Player/Player.cs
+++ Assets/Player/Player.cs
@@ -1,11 +1,13 @@
 using UnityEngine;
 
 public class Player : MonoBehaviour {
-  [HideInInspector] public PlayerSword Sword;
   [HideInInspector] public PlayerMove Move;
+  [HideInInspector] public PlayerSword Sword;
+  [HideInInspector] public PlayerHammer Hammer;
 
   private void Awake() {
     this.InitComponent(out Sword); Sword.Player = this;
     this.InitComponent(out Move); Move.Player = this;
+    this.InitComponent(out Hammer); Hammer.Player = this;
   }
 }
