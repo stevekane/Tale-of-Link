0e4b514a4695a79f60fb12918a3808fa32685d4e
Steve Kane <kanesteven@gmail.com>
2023-07-26 17:17:11 -0500

    fixed up some missing references for teleport

--- Assets/Tests/WallMover/WallCollider.cs
+++
@@ -1,7 +0,0 @@
-using UnityEngine;
-
-public class WallCollider : MonoBehaviour {
-  void OnTriggerEnter(Collider c) => SendMessage("OnWallTriggerEnter", c, SendMessageOptions.DontRequireReceiver);
-  void OnTriggerStay(Collider c) => SendMessage("OnWallTriggerStay", c, SendMessageOptions.DontRequireReceiver);
-  void OnTriggerExit(Collider c) => SendMessage("OnWallTriggerExit", c, SendMessageOptions.DontRequireReceiver);
-}
