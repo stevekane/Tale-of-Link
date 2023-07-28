bb293c0cfcf561518ce97ee5845ed8649216a600
Matt Perry <mpcomplete@gmail.com>
2023-07-28 18:24:15 -0400

    Split PathWanderer into Waypoints, PathTraversal

--- Assets/Paths/PathController.cs
+++ Assets/Paths/PathController.cs
@@ -1,14 +1,28 @@
 using UnityEngine;
 
 public class PathController : MonoBehaviour {
-  public PathWanderer PathWanderer;
+  public Waypoints Waypoints;
   public float MoveSpeed = 10f;
+  public PathTraversal.Modes Mode;
+
+  PathTraversal PathTraversal;
+
+  void Awake() {
+    PathTraversal = Waypoints.CreatePathTraversal(Mode);
+    transform.position = Waypoints.Nodes[0].transform.position;
+    transform.rotation = Waypoints.Nodes[0].transform.rotation;
+  }
 
   void FixedUpdate() {
     var pos = transform.position;
     var rotation = transform.rotation;
-    PathWanderer.Advance(ref pos, ref rotation, MoveSpeed);
+    PathTraversal.Advance(ref pos, ref rotation, MoveSpeed);
     transform.position = pos;
     transform.rotation = rotation;
+  }
+
+  void OnDrawGizmosSelected() {
+    var path = Waypoints.CreatePathTraversal(Mode);
+    path.DrawGizmos();
   }
 }
--- Assets/Paths/Waypoints.cs
+++ Assets/Paths/Waypoints.cs
@@ -2,11 +2,17 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 
-public class PathWanderer : MonoBehaviour {
+// Runtime data for a path traversal over a set of path nodes, featuring different traversal modes.
+public class PathTraversal {
   public enum Modes { Looping, BackAndForth };
-  public List<PathNode> Nodes;
-  public Modes Mode;
 
+  List<Segment> Segments = new();
+  int CurrentSegment = 0;
+
+  public PathTraversal(List<PathNode> nodes, Modes mode) {
+    BuildSegments(nodes, mode);
+    Segments[CurrentSegment].Begin();
+  }
   public void Advance(ref Vector3 pos, ref Quaternion rotation, float moveSpeed) {
     if (Segments[CurrentSegment].Advance(ref pos, ref rotation, moveSpeed)) {
       CurrentSegment = (CurrentSegment+1)%Segments.Count;
@@ -50,25 +56,45 @@ public class PathWanderer : MonoBehaviour {
     }
     public override Segment Reversed() => new SegmentWait { WaitTicks = WaitTicks };
   }
-  List<Segment> Segments = new();
-  int CurrentSegment = 0;
 
-  void Awake() {
-    Debug.Assert(Nodes.Count > 1, "Path with < 2 nodes don't make no sense ma dude");
-    for (var i=1; i < Nodes.Count; i++) {
-      Segments.Add(new SegmentTraverse { Start = Nodes[i-1].transform, End = Nodes[i].transform });
-      if (Nodes[i] is Waitpoint wait)
+  void BuildSegments(List<PathNode> nodes, Modes mode) {
+    Debug.Assert(nodes.Count > 1, "Path with < 2 nodes don't make no sense ma dude");
+    for (var i = 1; i < nodes.Count; i++) {
+      Segments.Add(new SegmentTraverse { Start = nodes[i-1].transform, End = nodes[i].transform });
+      if (nodes[i] is Waitpoint wait)
         Segments.Add(new SegmentWait { WaitTicks = wait.Duration.Ticks });
     }
-    if (Mode == Modes.Looping) {
-      Segments.Add(new SegmentTraverse { Start = Nodes[Nodes.Count-1].transform, End = Nodes[0].transform });
+    if (mode == Modes.Looping) {
+      Segments.Add(new SegmentTraverse { Start = nodes[nodes.Count-1].transform, End = nodes[0].transform });
     } else {
       var reversed = Segments.Select(s => s.Reversed()).Reverse();
       Segments.AddRange(reversed);
     }
+  }
 
-    transform.position = Nodes[0].transform.position;
-    transform.rotation = Nodes[0].transform.rotation;
-    Segments[CurrentSegment].Begin();
+  public void DrawGizmos() {
+    Gizmos.color = Color.yellow;
+    foreach (var s in Segments) {
+      if (s is SegmentTraverse t) {
+        var start = t.Start.position;
+        var end = t.End.position;
+        Gizmos.DrawLine(start, end);
+      } else if (s is SegmentWait w) {
+        // Draw a number?
+      }
+    }
+  }
+}
+
+public class Waypoints : MonoBehaviour {
+  public List<PathNode> Nodes;
+
+  public PathTraversal CreatePathTraversal(PathTraversal.Modes mode) {
+    return new(Nodes, mode);
+  }
+
+  void OnDrawGizmosSelected() {
+    var path = CreatePathTraversal(PathTraversal.Modes.BackAndForth);
+    path.DrawGizmos();
   }
 }
