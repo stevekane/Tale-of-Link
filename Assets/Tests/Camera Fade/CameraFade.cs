ff5965eada0fe9b7923b72d495c36d10e6d8bc0e
Steve Kane <kanesteven@gmail.com>
2023-07-29 16:41:25 -0500

    extensive fiddling with camera test setup... sorta busted atm

--- Assets/Tests/Camera Fade/CameraFade.cs
+++ Assets/Tests/Camera Fade/CameraFade.cs
@@ -2,22 +2,43 @@ using UnityEngine;
 
 [ExecuteAlways]
 public class CameraFade : MonoBehaviour {
-  [SerializeField] Camera Base;
-  [SerializeField] Camera Overlay;
+  [SerializeField] Transform Target;
+  [SerializeField] Camera CurrentFloor;
+  [SerializeField] Camera NextFloor;
   [SerializeField] Material FinalMaterial;
   [SerializeField] float FloorHeight = 4;
-  [Range(0,1)]
+  [SerializeField] int TargetFloor;
+  [SerializeField] int CameraFloor;
   [SerializeField] float Blend;
 
-  // Overlay camera only renders the current floor
-  // Base camera renders everything below the current floor
-  void Update() {
-    var floor = Mathf.FloorToInt(transform.position.y / FloorHeight);
-    Overlay.nearClipPlane = .02f;
-    Overlay.farClipPlane = transform.position.y % FloorHeight + .02f;
-    Base.nearClipPlane = Overlay.farClipPlane;
-    Base.farClipPlane = transform.position.y + .02f;
-    Blend = Mathf.Clamp01(transform.position.y > FloorHeight ? (transform.position.y % FloorHeight) : 1);
+  void LateUpdate() {
+    transform.position = Target.position + 6 * Vector3.up;
+    TargetFloor = Mathf.FloorToInt(Target.position.y / FloorHeight) + 1;
+    CameraFloor = Mathf.FloorToInt(transform.position.y / FloorHeight) + 1;
+    CurrentFloor.nearClipPlane = (transform.position.y-TargetFloor*FloorHeight) + .02f;
+    CurrentFloor.farClipPlane = transform.position.y + .02f;
+    NextFloor.nearClipPlane = .02f;
+    NextFloor.farClipPlane = CurrentFloor.nearClipPlane;
+    var floorBlendMin = FloorHeight-1;
+    var floorBlendMax = FloorHeight;
+    var floorPosition = Target.position.y % FloorHeight;
+    if (floorPosition <= floorBlendMax && floorPosition >= floorBlendMin) {
+      Blend = Mathf.InverseLerp(floorBlendMin, floorBlendMax, floorPosition);
+    } else if (CameraFloor == TargetFloor) {
+      Blend = 0;
+    } else {
+      Blend = 0;
+    }
     FinalMaterial.SetFloat("_Blend", Blend);
+  }
+
+  void OnDrawGizmos() {
+    Gizmos.color = Color.blue;
+    Gizmos.matrix = CurrentFloor.transform.localToWorldMatrix;
+    Gizmos.DrawFrustum(Vector3.zero, CurrentFloor.fieldOfView, CurrentFloor.farClipPlane, CurrentFloor.nearClipPlane, CurrentFloor.aspect);
+    var overlayColor = Color.red;
+    Gizmos.color = overlayColor;
+    Gizmos.matrix = NextFloor.transform.localToWorldMatrix;
+    Gizmos.DrawFrustum(Vector3.zero, NextFloor.fieldOfView, NextFloor.farClipPlane, NextFloor.nearClipPlane, NextFloor.aspect);
   }
 }
