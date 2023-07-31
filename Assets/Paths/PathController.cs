eb62c5d53cbc3e7258b50d02650050ed8024b280
Matt Perry <mpcomplete@gmail.com>
2023-07-31 18:09:25 -0400

    PathController.IsActive

--- Assets/Paths/PathController.cs
+++ Assets/Paths/PathController.cs
@@ -6,13 +6,15 @@ public class PathController : MonoBehaviour, IMoverController {
   public Waypoints Waypoints;
   public float MoveSpeed = 10f;
   public PathTraversal.Modes Mode;
+  public bool IsActive = true;
 
   PathTraversal PathTraversal;
 
   public void UpdateMovement(out Vector3 goalPosition, out Quaternion goalRotation, float deltaTime) {
     goalPosition = PhysicsMover.TransientPosition;
     goalRotation = PhysicsMover.TransientRotation;
-    PathTraversal.Advance(ref goalPosition, ref goalRotation, MoveSpeed);
+    if (IsActive)
+      PathTraversal.Advance(ref goalPosition, ref goalRotation, MoveSpeed);
   }
 
   void Start() {
