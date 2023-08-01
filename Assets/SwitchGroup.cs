3f415784f95676076a1e6bba91b9133415a19778
Steve Kane <kanesteven@gmail.com>
2023-07-31 23:24:47 -0500

    more changed to floor 3 adding switych groups and door

--- Assets/SwitchGroup.cs
+++ Assets/SwitchGroup.cs
@@ -1,12 +1,16 @@
-using System.Collections.Generic;
 using UnityEngine;
 
 public class SwitchGroup : MonoBehaviour {
   public int State = 0;
-  public List<Switch> Switches;
-  public List<SwitchBlock> SwitchBlocks;
+  public Switch[] Switches;
+  public SwitchBlock[] SwitchBlocks;
+  public bool FindAmongChildren;
 
   void Awake() {
+    if (FindAmongChildren) {
+      Switches = GetComponentsInChildren<Switch>();
+      SwitchBlocks = GetComponentsInChildren<SwitchBlock>();
+    }
     Switches.ForEach(s => s.GetComponent<Combatant>().OnHurt += OnSwitchHurt);
     Switches.ForEach(s => s.SetSwitchState(State, false));
     SwitchBlocks.ForEach(b => b.SetSwitchState(State, false));
