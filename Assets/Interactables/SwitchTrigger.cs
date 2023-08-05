3fa7bc59e2ad6cf01ba91e07a458f809c564338a
Matt Perry <mpcomplete@gmail.com>
2023-08-05 03:37:52 -0400

    SwitchTrigger

---
+++ Assets/Interactables/SwitchTrigger.cs
@@ -0,0 +1,23 @@
+using System.Linq;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class SwitchTrigger : MonoBehaviour {
+  public Switch2[] Switches;
+  public UnityEvent AllSwitchesOn;
+
+  void OnValidate() {
+    Switches = GetComponentsInChildren<Switch2>();
+  }
+  void Start() {
+    Switches.ForEach(s => s.GetComponent<Combatant>().OnHurt += (hit) => OnSwitchHurt(s));
+  }
+
+  void OnSwitchHurt(Switch2 s) {
+    s.SetSwitchState(1, true);
+    var numOn = Switches.Count(s => s.State == 1);
+    if (numOn == Switches.Length) {
+      AllSwitchesOn?.Invoke();
+    }
+  }
+}
