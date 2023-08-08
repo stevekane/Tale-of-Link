f9a2d006694e5d91d7c63c2b0b52f636fa18894b
Matt Perry <mpcomplete@gmail.com>
2023-08-08 12:46:15 -0400

    Raven very basic behavior

--- Assets/Core/Tasks/TaskManager.cs
+++ Assets/Core/Tasks/TaskManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Threading;
@@ -27,7 +28,11 @@ public class SimpleTaskScheduler : TaskScheduler {
       // (which Unity controls). If it's null, it'll use our current TaskScheduler when awaiting.
       SynchronizationContext.SetSynchronizationContext(null);
       while (Tasks.TryDequeue(out var task)) {
-        TryExecuteTask(task);
+        try {
+          TryExecuteTask(task);
+        } catch (Exception e) {
+          Debug.LogException(e);
+        }
       }
 
       NextTick.TrySetResult(true);
---
+++ Assets/Mobs/AIRaven.cs
@@ -0,0 +1,49 @@
+using System.Threading.Tasks;
+using UnityEngine;
+
+public class AIRaven : MonoBehaviour {
+  public AbilityManager AbilityManager;
+  public WorldSpaceMove Move;
+  public LayerMask SeeMask;
+  public Timeval WindupDuration;
+  public Timeval ChargeDuration;
+  public Animator Animator;
+
+  TaskScope Scope = new();
+
+  public void Start() {
+    Scope.Start(Waiter.Repeat(Behavior));
+  }
+
+  void OnDestroy() {
+    Scope.Dispose();
+  }
+
+  async Task Behavior(TaskScope scope) {
+    var player = PlayerManager.Instance.Player;
+    await scope.Until(() => PlayerManager.Instance.Player && CanSee(PlayerManager.Instance.Player.transform));
+    Animator.SetTrigger("Awake");
+    GetComponent<WorldSpaceController>().Unground();
+    await scope.Delay(WindupDuration);
+    await scope.Repeat(ChargeAtPlayer);
+  }
+
+  bool CanSee(Transform target) {
+    return target.IsVisibleFrom(transform.position, SeeMask);
+  }
+
+  async Task ChargeAtPlayer(TaskScope scope) {
+    await scope.Until(() => AbilityManager.CanRun(Move.Move));
+    await scope.Any(
+      Waiter.Delay(ChargeDuration),
+      async s => {
+        var player = PlayerManager.Instance.Player;
+        var dir = (player.transform.position - transform.position).normalized;
+        Debug.Log($"Charging: {dir}");
+        while (true) {
+          AbilityManager.Run(Move.Move, dir);
+          await scope.Tick();
+        }
+      });
+  }
+}
