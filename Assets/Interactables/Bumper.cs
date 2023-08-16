eec163700c154dbb225280aa5a2e2fdcd6a90dc5
Matt Perry <mpcomplete@gmail.com>
2023-08-15 22:46:08 -0400

    Bumper improvements: scale bug, knockback

--- Assets/Interactables/Bumper.cs
+++ Assets/Interactables/Bumper.cs
@@ -7,17 +7,33 @@ public class Bumper : MonoBehaviour {
   public Collider Collider;
   public float BumpForce = 10f;
 
+  Vector3 BaseScale;
+  void Start() {
+    GetComponentInParent<Combatant>().OnHurt += OnHurt;
+    BaseScale = Model.localScale;
+  }
+
+  void OnHurt(HitEvent obj) {
+    StopAllCoroutines();
+    StartCoroutine(Animate());
+  }
+
   void OnTriggerEnter(Collider other) {
-    if (other.TryGetComponent(out WorldSpaceController controller)) {
-      Bump(controller);
+    if (other.TryGetComponent(out AbilityManager am)) {
+      Bump(am);
     }
   }
 
-  void Bump(WorldSpaceController controller) {
+  void Bump(AbilityManager am) {
     StopAllCoroutines();
     StartCoroutine(Animate());
-    var delta = controller.Position - transform.position;
-    controller.PhysicsVelocity += BumpForce * delta.normalized;
+    var knockback = am.Abilities.Find(a => a is Knockback) as Knockback;
+    if (knockback) {
+      var delta = am.transform.position - transform.position;
+      knockback.Run(BumpForce * delta.XZ().normalized);
+    }
+    //var delta = controller.Position - transform.position;
+    //controller.PhysicsVelocity += BumpForce * delta.normalized;
   }
 
   public float AnimationDecay = 1f;
@@ -25,15 +41,14 @@ public class Bumper : MonoBehaviour {
   public float AnimationFrequency = 3f;
   IEnumerator Animate() {
     var t = 0f;
-    var origScale = Model.localScale;
     while (true) {
       var decay = Mathf.Exp(-AnimationDecay * t);
       if (decay < .1f)
         break;
-      Model.localScale = origScale + AnimationAmplitude * decay * Mathf.Sin(AnimationFrequency * t) * Vector3.one;
+      Model.localScale = BaseScale + AnimationAmplitude * decay * Mathf.Sin(AnimationFrequency * t) * Vector3.one;
       t += Time.fixedDeltaTime;
       yield return new WaitForFixedUpdate();
     }
-    Model.localScale = origScale;
+    Model.localScale = BaseScale;
   }
 }
