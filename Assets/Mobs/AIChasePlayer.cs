a4c0e217988925ac5aca3837e0a597b40705845d
Matt Perry <mpcomplete@gmail.com>
2023-08-18 16:27:51 -0400

    AIChase only chase if on same floor

--- Assets/Mobs/AIChasePlayer.cs
+++ Assets/Mobs/AIChasePlayer.cs
@@ -3,11 +3,15 @@ using UnityEngine;
 public class AIChasePlayer : MonoBehaviour {
   public AbilityManager AbilityManager;
   public WorldSpaceMove Move;
+  //public LayerMask SeeMask;
+
+  // Target is visible if he's on the same floor. Ish.
+  Transform Target => PlayerManager.Instance.MobTarget ? PlayerManager.Instance.MobTarget.transform : null;
+  bool CanSeeTarget => Target && Mathf.Abs(Target.position.y - transform.position.y) < 2f;
 
   void FixedUpdate() {
-    var player = PlayerManager.Instance.MobTarget;
-    if (player && AbilityManager.CanRun(Move.Move)) {
-      var dir = (player.transform.position - transform.position).XZ().normalized;
+    if (CanSeeTarget && AbilityManager.CanRun(Move.Move)) {
+      var dir = (Target.position - transform.position).XZ().normalized;
       AbilityManager.Run(Move.Move, dir);
     } else {
       AbilityManager.Run(Move.Move, Vector3.zero);
