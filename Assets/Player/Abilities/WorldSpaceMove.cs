ac04671b1042d306eff20032af0b654acb3258cd
Matt Perry <mpcomplete@gmail.com>
2023-08-18 16:21:26 -0400

    AI avoid wall

--- Assets/Player/Abilities/WorldSpaceMove.cs
+++ Assets/Player/Abilities/WorldSpaceMove.cs
@@ -23,8 +23,8 @@ public class WorldSpaceMove : Ability {
     if (LedgeStopDistance > 0f) {
       var probePos = Controller.Position + LedgeStopDistance*dir.normalized;
       if (Controller.Motor.CharacterCollisionsRaycast(probePos + .5f*Vector3.up, Vector3.down, .6f, out var hit, null) == 0) {
+        // Heading towards a ledge.
         OnLedge?.Invoke();
-        Debug.Log($"Ledge void at {probePos}");
         var voidPos = probePos - .1f*Vector3.up;
         if (Controller.Motor.CharacterCollisionsRaycast(voidPos, -dir.normalized, LedgeStopDistance, out var ledgeHit, null) > 0) {
           var towardsLedge = Vector3.Project(dir, ledgeHit.normal.XZ());
@@ -33,6 +33,11 @@ public class WorldSpaceMove : Ability {
           Debug.LogWarning($"No ledge detected between ground and void.");
           //dir = Vector3.zero;
         }
+      } else if (Controller.Motor.CharacterCollisionsRaycast(Controller.Position + .1f*Vector3.up, dir.normalized, LedgeStopDistance, out var wallHit, null) > 0) {
+        // Heading towards a wall.
+        OnLedge?.Invoke();  // ledge edge, potato potato
+        var towardsWall = Vector3.Project(dir, -wallHit.normal.XZ());
+        dir -= towardsWall.XZ();
       }
     }
     Controller.MaxMoveSpeed = Speed;
