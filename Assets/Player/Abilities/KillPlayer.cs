f6a33ce277f9964d3aa2626693cba6b1ecd90d10
Matt Perry <mpcomplete@gmail.com>
2023-08-21 15:11:24 -0400

    Restart UI

--- Assets/Player/Abilities/KillPlayer.cs
+++ Assets/Player/Abilities/KillPlayer.cs
@@ -6,6 +6,7 @@ public class KillPlayer : ClassicAbility {
   [SerializeField] bool HasFairy;
   [SerializeField] float FadeSpeed = 1;
   [SerializeField] Timeval DyingDuration = Timeval.FromSeconds(2);
+  [SerializeField] HUD HUD;
 
   public override async Task MainAction(TaskScope scope) {
     try {
@@ -19,6 +20,9 @@ public class KillPlayer : ClassicAbility {
       TimeManager.Instance.Frozen = true;
       Debug.Log(LocalTime.TimeScale);
       Debug.Log("Predying");
+      if (!HasFairy) {
+        HUD.ShowGameOver();
+      }
       await scope.Ticks(DyingDuration.Ticks);
       Debug.Log("Postdying");
       if (HasFairy) {
@@ -30,6 +34,7 @@ public class KillPlayer : ClassicAbility {
         Debug.Log("Postviving");
         killable.Alive = true;
         CameraManager.Instance.FadeIn(FadeSpeed);
+        HasFairy = false;
       } else {
         killable.Dead = true;
       }
--- Assets/UI/HUD.cs
+++ Assets/UI/HUD.cs
@@ -1,9 +1,16 @@
+using System;
+using System.Collections;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 [RequireComponent(typeof(Canvas))]
 public class HUD : MonoBehaviour {
   [SerializeField] Killable Killable;
   [SerializeField] CanvasGroup HUDCanvasGroup;
+  [SerializeField] CanvasGroup WastedCanvasGroup;
+  [SerializeField] Button RestartButton;
   [SerializeField] CanvasGroup CollectionInfoCanvasGroup;
   [SerializeField] CollectionInfo CollectionInfo;
 
@@ -24,11 +31,27 @@ public class HUD : MonoBehaviour {
     CollectionInfoCanvasGroup.alpha = 0;
   }
 
+  public void ShowGameOver() {
+    RestartButton.gameObject.SetActive(true);
+    EventSystem.current.SetSelectedGameObject(RestartButton.gameObject);
+    StartCoroutine(FadeWasted());
+  }
+
+  const float AlphaSpeed = .01f;
+  IEnumerator FadeWasted() {
+    while (WastedCanvasGroup.alpha < 1f) {
+      WastedCanvasGroup.alpha += AlphaSpeed;
+      yield return new WaitForFixedUpdate();
+    }
+  }
+
   void Start() {
     GetComponent<Canvas>().worldCamera = CameraManager.Instance.Camera;
     Killable.OnDying += OnDying;
     Killable.OnSpawning += OnSpawning;
+    WastedCanvasGroup.alpha = 0;
     HideCollectionInfo();
+    RestartButton.onClick.AddListener(OnRestart);
   }
 
   void OnDestroy() {
@@ -42,5 +65,9 @@ public class HUD : MonoBehaviour {
 
   void OnDying() {
     HUDCanvasGroup.alpha = 0;
+  }
+
+  void OnRestart() {
+    SceneManager.LoadScene(SceneManager.GetActiveScene().name);
   }
 }
